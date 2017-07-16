180090f6996ab411a28388b8695eb4a87337cf1e
mlboua <b.oua55@hotmail.fr>
2017-07-16 19:26:30 +0200

    feat(core): add FOSUser configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -15,6 +15,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            //new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new UserBundle\UserBundle(),
+            new TaamaBundle\TaamaBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
