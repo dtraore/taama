ee9f7b2858d771982db051a2033cee2b6af6409e
DIADIE TRAORE <dtraore@diadie-traore.net>
2017-07-31 23:04:28 +0200

    organisation des assets

--- src/TaamaBundle/Controller/HomeController.php
+++ src/TaamaBundle/Controller/HomeController.php
@@ -12,6 +12,17 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
+        dump('Yes');
+        dump($this->getParameter('kernel.environment'));
         return $this->render('TaamaBundle:home:index.html.twig');
+    }
+
+    /**
+     * @Route("/test")
+     */
+    public function testAction()
+    {
+        //dump('Yes');
+        return $this->render('TaamaBundle:base.html.twig');
     }
 }
